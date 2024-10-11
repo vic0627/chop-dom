b9d9fc478fd7fd482610c97cb0424e1f302dcae9
VICTOR HSU <pure90719@gmail.com>
2024-10-11 12:27:00 +0800

    add: typescript-eslint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+];
